0cd917f1973e60a6b4edb6653a9dc4db59f0e493
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-04-28 20:54:50 -0500

    small changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 let fetchResturant = function (foodZip, foodType) {
-    foodApiAddress = 'https://api.documenu.com/v2/restaurants/zip_code/' + foodZip + '?size=1&cuisine=' + foodType + '&key=983626163e2a685b3ade4ddc277fc658'
+    foodApiAddress = 'https://api.documenu.com/v2/restaurants/zip_code/' + foodZip + '?size=5&cuisine=' + foodType + '&key=983626163e2a685b3ade4ddc277fc658'
     fetch(foodApiAddress)
         .then(function (foodResponse) {
             foodResponse.json().then(function (foodData) {
                 console.log(foodData);
             })
         })
-}
+};
 
-fetchResturant('78728', 'american')
+fetchResturant('78728', 'american');

da2a5cc3faf2f9249e946dbc83a38d56a777cb90
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-04-30 09:47:02 -0500

    Added select to html and uery selector to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 // We Are awesome 
+let genreEl = document.querySelector('#genre')
+
 var genreSelector = function () {
   fetch('https://api.themoviedb.org/3/discover/movie?api_key=f0c90416c29040e056b30db72789fae5&with_genres=16&language=en-US')
 
@@ -26,13 +28,17 @@ let fetchResturant = function (foodZip, foodType) {
         randomFood = randomNumGen();
         foodName = foodData.data[randomFood].restaurant_name;
         foodSite = foodData.data[randomFood].restaurant_website;
-        let foodButton = document.createElement('a')
-        foodButton.setAttribute('href', foodSite);
-        foodButton.setAttribute('target', '_blank');
-        foodButton.textContent = foodName;
+        createFoodLink()
       })
     })
 };
+
+let createFoodLink = function () {
+  foodButton = document.createElement('a')
+  foodButton.setAttribute('href', foodSite);
+  foodButton.setAttribute('target', '_blank');
+  foodButton.textContent = foodName;
+}
 
 let randomNumGen = function () {
   return Math.floor(Math.random() * 5);

41d43657f301d0bce19bf4f67c069075c31b0e33
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-05-02 09:37:30 -0500

    Created Save and load functions added to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,44 +4,45 @@ let zipRequestEl = document.querySelector('#zipRequest');
 let zipSubmitEl = document.querySelector('#zipSubmit');
 let zipFormEl = document.querySelector('#zipForm')
 let searchHistory = [];
+let cuisinelist = ['Sandwiches', 'American', 'Bar Food', 'Italian', 'Mexican', 'Pizza', 'Dali Food', 'Japanese']
 
-var genreSelector = function (genre) {
-  fetch('https://api.themoviedb.org/3/discover/movie?api_key=f0c90416c29040e056b30db72789fae5&with_genres=' + genre + '&language=en-US')
+// var genreSelector = function (genre) {
+//   fetch('https://api.themoviedb.org/3/discover/movie?api_key=f0c90416c29040e056b30db72789fae5&with_genres=' + genre + '&language=en-US')
 
-    .then(response => response.json())
+//     .then(response => response.json())
 
-    .then(data => console.log(data))
-}
+//     .then(data => console.log(data))
+// }
 
-var genreList = function () {
-  fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=f0c90416c29040e056b30db72789fae5&language=en-US')
+// var genreList = function () {
+//   fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=f0c90416c29040e056b30db72789fae5&language=en-US')
 
-    .then(response => response.json())
+//     .then(response => response.json())
 
-    .then(data => console.log(data))
-}
+//     .then(data => console.log(data))
+// }
 
-let fetchResturant = function (foodZip, foodType) {
-  foodApiAddress = 'https://api.documenu.com/v2/restaurants/zip_code/' + foodZip + '?size=5&cuisine=' + foodType + '&key=983626163e2a685b3ade4ddc277fc658'
-  fetch(foodApiAddress)
-    .then(function (foodResponse) {
-      if (foodResponse.ok) {
-        foodResponse.json().then(function (foodData) {
-          if (foodData.data[0]) {
-            randomFood = randomNumGen();
-            foodName = foodData.data[randomFood].restaurant_name;
-            foodSite = foodData.data[randomFood].restaurant_website;
-            createFoodLink()
-          }
-          else {
-            console.log('No results')
-          }
-        })
-      } else {
-        alert(foodResponse.statusText)
-      }
-    })
-};
+// let fetchResturant = function (foodZip, foodType) {
+//   foodApiAddress = 'https://api.documenu.com/v2/restaurants/zip_code/' + foodZip + '?size=5&cuisine=' + foodType + '&key=983626163e2a685b3ade4ddc277fc658'
+//   fetch(foodApiAddress)
+//     .then(function (foodResponse) {
+//       if (foodResponse.ok) {
+//         foodResponse.json().then(function (foodData) {
+//           if (foodData.data[0]) {
+//             randomFood = randomNumGen(foodData.data.length);
+//             foodName = foodData.data[randomFood].restaurant_name;
+//             foodSite = foodData.data[randomFood].restaurant_website;
+//             createFoodLink()
+//           }
+//           else {
+//             console.log('No results')
+//           }
+//         })
+//       } else {
+//         alert(foodResponse.statusText)
+//       }
+//     })
+// };
 
 let createFoodLink = function () {
   foodButton = document.createElement('a')
@@ -50,8 +51,8 @@ let createFoodLink = function () {
   foodButton.textContent = foodName;
 }
 
-let randomNumGen = function () {
-  return Math.floor(Math.random() * 5);
+let randomNumGen = function (max) {
+  return Math.floor(Math.random() * max);
 };
 
 let pullZip = function () {
@@ -60,13 +61,29 @@ let pullZip = function () {
   zipRequestEl.value = "";
 };
 
+let saveHistory = function () {
+  historyString = JSON.stringify(searchHistory)
+  localStorage.setItem('history', historyString)
+}
+
+let loadHistory = function () {
+  let searchHistory = JSON.parse(localStorage.getItem('history'));
+  if (searchHistory) {
+    return
+  }
+  searchHistory = [];
+}
+
 let handleSelection = function () {
-  console.log(genreEl.value);
-  console.log(zipCode);
-  genreSelector(genreEl.value);
-  fetchResturant(zipCode, 'american')
+  // genreSelector(genreEl.value);
+  randomCusine = randomNumGen(cuisinelist.length);
+  // fetchResturant(zipCode, cuisinelist[randomCusine])
+  let addSave = { genreType: genreEl.value, cusineType: cuisinelist[randomCusine] };
+  searchHistory.push(addSave);
+  saveHistory();
 };
 
 genreEl.addEventListener('change', handleSelection)
 zipFormEl.addEventListener('submit', pullZip)
-genreList()
+// genreList()
+loadHistory();
